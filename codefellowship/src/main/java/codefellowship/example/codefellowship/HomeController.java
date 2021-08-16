aa39f4df69075dad845a187dca55dc98339f79a7
Victoria-FZ-eng <vzulof@gmail.com>
2021-08-16 17:54:34 +0300

    1

--- codefellowship/src/main/java/codefellowship/example/codefellowship/HomeController.java
+++ codefellowship/src/main/java/codefellowship/example/codefellowship/HomeController.java
@@ -11,25 +11,15 @@ import java.security.Principal;
 
 @Controller
 public class HomeController {
-    // inside a controller class, you will add the endpoints (requests mapping)
 
-    // as we have thymeleaf configuration, then the app expects to receive template names in responses
+    @GetMapping("/hello")    // localhost:8080
+    @ResponseBody
+    public String getHome(){
 
-    // create an endpoint that corresponds to get requests
-    // add an implementation to the function that will run when receiving a request on that path
-//    @GetMapping("/")    // localhost:8080    localhost:8080/
-//    @ResponseBody       // include the return value as is in the response body
-//    public String getHome(Principal p){
-////        m.addAttribute("userName", p.getName());
-//        return "Hello World " + p.getName();
-//    }
+        return "Hello";
+    }
 
-//    @GetMapping("/test")
-//    public String testTemplate(Model model){
-//        model.addAttribute("Name","Humam");
-//        model.addAttribute("Age", 25);
-//        return "template.html";
-//    }
+
 
 
 }
