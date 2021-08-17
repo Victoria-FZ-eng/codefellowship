c376685b8d62540585ea8aebefa4b689b852300a
Victoria-FZ-eng <vzulof@gmail.com>
2021-08-17 21:08:23 +0300

    link

--- codefellowship/src/main/java/codefellowship/example/codefellowship/ApplicationUserController.java
+++ codefellowship/src/main/java/codefellowship/example/codefellowship/ApplicationUserController.java
@@ -3,10 +3,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -22,38 +19,16 @@ public class ApplicationUserController {
         return "home.html";
     }
 
-    @GetMapping("/profile")
-    public String profile(@RequestParam(value = "username")String  username,Model m){
-
-        m.addAttribute("user", applicationUserRepository.findByUsername(username));
-        return "profile.html";
+    @GetMapping("/in")
+    public String homeIn(){
+        return "homeIn.html";
     }
 
-
-    @GetMapping("/hello")
-    @ResponseBody
-    public String getHome(){
-
-        return "Hello";
-    }
 
     @GetMapping("/signup")
     public String getSignUpPage(){
         return "signUp.html";
     }
-
-    @PostMapping("/login")
-    public String goToProfile(Model m,@RequestParam(value="username") String username){
-        boolean isLoggedIn= false;
-        ApplicationUser user = applicationUserRepository.findByUsername(username);
-        if(applicationUserRepository.findByUsername(username) != null){
-            m.addAttribute("user",user);
-            isLoggedIn = true;
-        }
-        m.addAttribute("isLoggedIn",isLoggedIn);
-        return "profile.html";
-    }
-
     @GetMapping("/login")
     public String getSignInPage(){
         return "signIn.html";
@@ -64,6 +39,53 @@ public class ApplicationUserController {
         ApplicationUser newUser = new ApplicationUser(username,bCryptPasswordEncoder.encode(password),firstName,lastName, bio, dateOfBirth);
         applicationUserRepository.save(newUser);
         return new RedirectView("/login");
+    }
+
+    @PostMapping("/login")
+    public String goToHomePage(Model m,@RequestParam(value="username") String username){
+        boolean isLoggedIn= false;
+        ApplicationUser user = applicationUserRepository.findByUsername(username);
+        if(applicationUserRepository.findByUsername(username) != null){
+            m.addAttribute("user",user);
+            isLoggedIn = true;
+        }
+        m.addAttribute("isLoggedIn",isLoggedIn);
+        return "homeIn.html";
+    }
+
+
+
+
+
+
+    @GetMapping("/allUsers")
+    public String viewUsers(Model m, @RequestParam(value = "name")String username){
+
+      //  System.out.println("-------------------------------inside allUsers--------------------------------");
+        m.addAttribute("allUsers",applicationUserRepository.findAll());
+        m.addAttribute("theUser",applicationUserRepository.findByUsername(username));
+        return "allUsers.html";
+    }
+
+    @GetMapping("/profile")
+    public String profile(@RequestParam(value = "name")String  username,Model m){
+       // System.out.println("-------------------------------inside profile--------------------------------");
+        m.addAttribute("user", applicationUserRepository.findByUsername(username));
+        return "profile.html";
+    }
+
+    @GetMapping("/profile/id")
+    public String userPro(Model m,@RequestParam(value="id") Integer id){
+        ApplicationUser user = applicationUserRepository.findById(id).get();
+        m.addAttribute("user",user);
+        return "profile.html";
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String getHome(){
+        //System.out.println("-----------------------helloooooooooooooo");
+        return "Hello";
     }
 
 }
